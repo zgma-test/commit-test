f961fccc2cc015213d6ca5f60cd8923fdd9ac885
sigmavcs <jchamath+sigma@gmail.com>
2019-09-10 11:25:18 +0530

    chamath

--- dwdwd/lambda.js
+++ dwdwd/lambda.js
@@ -1,5 +1,5 @@
 exports.handler = function(event, context, callback) {
     
-    console.log("Hello World");
+    console.log("Hello World!!!");
     callback(null, {"message": "Successfully executed"});
 }
